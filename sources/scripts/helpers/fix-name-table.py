181c055c39b62f6057187dd74e385d8b8c5c0816
Eli Heuer <elih@member.fsf.org>
2019-09-12 18:00:30 -0400

    Update name table fix script after testing with main build script

--- sources/scripts/helpers/fix-name-table.py
+++ sources/scripts/helpers/fix-name-table.py
@@ -9,27 +9,26 @@ parser.add_argument('ttf_font', help="Font in OpenType (TTF) format")
 def main():
     args = parser.parse_args()
     font = ttLib.TTFont(args.ttf_font)
+
     print('[INFO] Fixing name table...')
     #print(font)
     #print(font['name'])
     #nameTable = font['name'].names
     #print(nameTable)
-    print('[INFO] Adding new records...')
 
+    print('[INFO] Adding new records...')
     sixteen = NameRecord()
     sixteen.nameID = 16
     sixteen.platformID = 0
     sixteen.platEncID = 1
     sixteen.langID = 0x409
     sixteen.string = 'Lemonada'
-
     seventeen = NameRecord()
     seventeen.nameID = 17
     seventeen.platformID = 0
     seventeen.platEncID = 1
     seventeen.langID = 0x409
     seventeen.string = 'Regular'
-
     font['name'].names.append(sixteen)
     font['name'].names.append(seventeen)
     font.save(args.ttf_font)
